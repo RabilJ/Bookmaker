4c41be19e284320e57af3a577c731df5f6db4cf7
Jakub <jakub.popielarski7@gmail.com>
2019-02-18 13:55:35 +0100

    A start

---
+++ src/main/java/com/example/cfs/CfsApplication.java
@@ -0,0 +1,13 @@
+package com.example.cfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfsApplication.class, args);
+    }
+
+}
